33df4675ffe027d3b4bc47c47505daaa19fe3d12
us_mcquenn <sherzoduronov032@gmail.com>
2023-05-16 20:40:28 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        List<ConsoleColor> colors = new List<ConsoleColor>();
+        colors.Add(ConsoleColor.Green);
+        colors.Add(ConsoleColor.Blue);
+        colors.Add(ConsoleColor.Yellow);
+        colors.Add(ConsoleColor.Red);
+        colors.Add(ConsoleColor.Magenta);
+        colors.Add(ConsoleColor.Cyan);
+        Random ran = new Random();
+
+        Write("Ismingizni kiriting: ");
+        string? name =  ReadLine();
+        WriteLine(name);
+        while(true)
+        {
+            foreach(char c in name)
+            {
+                ForegroundColor = colors[ran.Next(colors.Count)];
+                Write($"{c} ");
+            }
+        }
+    }
+}
